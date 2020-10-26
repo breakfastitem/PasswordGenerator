bfcc88ce2ee4e22d61574256089c7a2f7f413e72
breakfastitem <andrew.ehrman@gmail.com>
2020-10-26 15:27:39 -0400

    Added object to contain parameters. Declared generatePassword and prmpt parameters function.

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Object that hold password criteria
+var passwordParameters = {
+  //Constants
+  MAXLENGTH : 128,
+  MINLENGTH : 8,
+
+  //Prompted parameters
+  length : 0,
+  lowerCase : false,
+  upperCase : false,
+  numerics : false,
+  specialChar : false,
+
+  //TODO: Prompt ALL parameters
+  promptParameters(){
+    var validInput=true;
+
+    var holder = prompt("");
+
+  }
+
+};
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+}
+//TODO: Genrate password based on parameters
+//generates password based on passwordParameters object
+function generatePassword(passwordParameters) {
 
 }
 
